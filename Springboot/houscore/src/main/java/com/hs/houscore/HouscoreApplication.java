59bc0c3a045ca88d184774b33c9e2327fdf1c201
박영규 <dudrb5260@naver.com>
2024-04-17 15:16:59 +0900

    Init: be/dev

---
+++ Springboot/houscore/src/main/java/com/hs/houscore/HouscoreApplication.java
@@ -0,0 +1,13 @@
+package com.hs.houscore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouscoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouscoreApplication.class, args);
+	}
+
+}
---
+++ Springboot/houscore/src/test/java/com/hs/houscore/HouscoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hs.houscore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouscoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
