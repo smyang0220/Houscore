897b7abffc18290ca1c362d1d05e5606391e4ea3
박영규 <dudrb5260@naver.com>
2024-04-23 17:51:43 +0900

    Feat: postgre member controller, entity 작업

--- Springboot/houscore/src/main/java/com/hs/houscore/postgre/entity/MemberEntity.java
+++ Springboot/houscore/src/main/java/com/hs/houscore/postgre/entity/MemberEntity.java
@@ -1,17 +1,14 @@
 package com.hs.houscore.postgre.entity;
 
 import com.hs.houscore.oauth2.member.OAuth2Provider;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import java.util.Date;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
@@ -22,11 +19,28 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class MemberEntity {
     @Id
     private String memberEmail;
+    private String name;
     private String memberName;
     private String password;
     private String profileImage;
+    private String age_range;
+    private String social;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender; // 성별 enum 타입
+
+    @Enumerated(EnumType.STRING)
+    private Role role; // 역할 enum 타입
+
     @Enumerated(EnumType.STRING)
     private OAuth2Provider provider;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt; // 생성 날짜
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updatedAt; // 업데이트 날짜
+
     private String refreshToken;
 
     public void passwordEncode(PasswordEncoder passwordEncoder) {
@@ -35,5 +49,27 @@ public class MemberEntity {
 
     public void updateRefreshToken(String updateRefreshToken) {
         this.refreshToken = updateRefreshToken;
+    }
+
+    // 엔티티가 영속성 컨텍스트에 저장되기 전에 호출됩니다.
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = new Date();
+    }
+
+    // 엔티티가 영속성 컨텍스트에 merge 될 때 호출됩니다.
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = new Date();
+    }
+
+    // 성별을 위한 Enum 타입
+    public enum Gender {
+        MAN, WOMAN
+    }
+
+    // 역할을 위한 Enum 타입
+    public enum Role {
+        MEMBER, ADMIN
     }
 }
