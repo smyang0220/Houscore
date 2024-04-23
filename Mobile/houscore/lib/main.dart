60522e81d9e030a7eb54aee201606262852024f3
양희태 <smyang0220@naver.com>
2024-04-23 17:54:49 +0900

    feat : 카카오로그인  sdk 추가 및 구현

--- Mobile/houscore/lib/main.dart
+++ Mobile/houscore/lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart'; // SystemChrome을 사용하기 위해 필요
+import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 import 'common/view/splash_screen.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized(); // Flutter 바인딩 초기화
+
+  KakaoSdk.init(
+    nativeAppKey: '9be0dbc80cce4d34bd4dae40010dacb6',
+    javaScriptAppKey: '6291268ed3dc5224d56ed06634b9942c',
+  );
+
   SystemChrome.setPreferredOrientations([ // 화면 방향 설정
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
