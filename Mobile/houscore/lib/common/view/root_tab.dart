91020e310ef79ffb65af587b6192cd8ce2227324
양희태 <smyang0220@naver.com>
2024-04-25 14:04:24 +0900

    Build : build.gradle dependencies 삭제 및 주소기능 예시코드추가

--- Mobile/houscore/lib/common/view/root_tab.dart
+++ Mobile/houscore/lib/common/view/root_tab.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:houscore/common/const/color.dart';
 import 'package:houscore/common/layout/default_layout.dart';
+import 'package:remedi_kopo/remedi_kopo.dart';
 
 class RootTab extends StatefulWidget {
   const RootTab({super.key});
@@ -48,7 +50,7 @@ class _RootTabState extends State<RootTab>
         physics: NeverScrollableScrollPhysics(), // 안움직이게 만듬
         children: [
           Center(child: Container(child: Text('홈'))),
-          Center(child: Container(child: Text('음식'))),
+          KakaoAddressScreen(),
           Center(child: Container(child: Text('주문'))),
           Center(child: Container(child: Text('프로필'))),
         ],
@@ -84,5 +86,99 @@ class _RootTabState extends State<RootTab>
       ],
        ),
     );
+  }
+}
+
+
+class KakaoAddressScreen extends StatelessWidget {
+  KakaoAddressScreen({super.key});
+
+  /// Form State
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  Map<String, String> formData = {};
+
+  /// Controller
+  final TextEditingController _postcodeController = TextEditingController();
+  final TextEditingController _addressController = TextEditingController();
+  final TextEditingController _addressDetailController =
+  TextEditingController();
+
+  Widget _gap() {
+    return const SizedBox(
+      height: 10,
+    );
+  }
+
+  void _searchAddress(BuildContext context) async {
+    KopoModel? model = await Navigator.push(
+      context,
+      CupertinoPageRoute(
+        builder: (context) => RemediKopo(),
+      ),
+    );
+
+    if (model != null) {
+      final postcode = model.zonecode ?? '';
+      _postcodeController.value = TextEditingValue(
+        text: postcode,
+      );
+      formData['postcode'] = postcode;
+
+      final address = model.address ?? '';
+      _addressController.value = TextEditingValue(
+        text: address,
+      );
+      formData['address'] = address;
+
+      final buildingName = model.buildingName ?? '';
+      _addressDetailController.value = TextEditingValue(
+        text: buildingName,
+      );
+      formData['address_detail'] = buildingName;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+        return Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Form(
+              key: _formKey,
+              child: Column(
+                children: <Widget>[
+                  TextFormField(
+                    controller: _postcodeController,
+                    decoration: const InputDecoration(
+                      hintText: '우편번호',
+                    ),
+                    readOnly: true,
+                  ),
+                  _gap(),
+                  TextFormField(
+                    controller: _addressController,
+                    decoration: const InputDecoration(
+                      hintText: '기본주소',
+                    ),
+                    readOnly: true,
+                  ),
+                  _gap(),
+                  TextFormField(
+                    textInputAction: TextInputAction.done,
+                    controller: _addressDetailController,
+                    decoration: const InputDecoration(
+                      hintText: '상세주소 입력',
+                    ),
+                  ),
+                  _gap(),
+                  CupertinoButton(
+                    onPressed: () => _searchAddress(context),
+                    child: const Text('주소검색'),
+                  )
+                ],
+              ),
+            ),
+          ],
+        );
   }
 }
