4e62639fd2944aa97cffec97582d05553b92f692
ansangjun <yonggari0821@naver.com>
2024-04-29 09:44:02 +0900

    Feat: AI 추천 추가 및 메인화면 구현

---
+++ Mobile/houscore/lib/common/component/search_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SearchPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("주소 검색"),
+      ),
+      body: Center(
+        child: Text("여기에 주소 검색 기능을 구현하세요."),
+      ),
+    );
+  }
+}
