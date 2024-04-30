fe144f899e2e52bff2490951a1ac4f4a5cc4f214
김희주 <mary2946@naver.com>
2024-04-30 12:43:51 +0900

    Feat: 내 정보 관리 관심 지역 등록 구현 & 등록일 수정일 Entity 생성 및 적용

---
+++ Springboot/houscore/src/main/java/com/hs/houscore/postgre/entity/MyInterestedAreaEntity.java
@@ -0,0 +1,21 @@
+package com.hs.houscore.postgre.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Data
+@Builder
+@Getter
+@Table(name = "my_interested_area")
+@SequenceGenerator(name="my_interested_area_seq", sequenceName="my_interested_area_seq", initialValue=1, allocationSize=1)
+public class MyInterestedAreaEntity extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long areaId;
+    private String memberId;
+    private String address;
+}
