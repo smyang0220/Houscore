6677b5fddcc55b4964a8cd4d12a32b7b6307a460
ansangjun <yonggari0821@naver.com>
2024-05-02 13:30:20 +0900

    Feat : 주변 생활시설 구현 및 주변인프라와 교통 수정

--- Mobile/houscore/lib/residence/component/nearby_infra.dart
+++ Mobile/houscore/lib/residence/component/nearby_infra.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:houscore/common/const/color.dart';
+import 'package:houscore/residence/utils/place_utils.dart';
 
 import '../model/nearby_infra_model.dart';
 
@@ -16,11 +17,56 @@ class NearbyInfra extends StatefulWidget {
 }
 
 class _NearbyInfraState extends State<NearbyInfra> {
-  bool _showHospitals = true;
+  InfraType? _selectedType;
 
   @override
   Widget build(BuildContext context) {
-    List<Infra> filteredList = widget.infraItems.where((item) => item.hospitalOrPark == _showHospitals).toList();
+    List<Infra> filteredList = _selectedType == null ? widget.infraItems : widget.infraItems.where((item) => item.type == _selectedType).toList();
+
+    // 버튼 구성
+    Widget buttonContent(String iconPath, String label) {
+      return Padding(
+        padding: const EdgeInsets.symmetric(vertical: 1, horizontal: 24),
+        child: Row(
+          children: [
+            Image.asset(iconPath, width: 15),
+            SizedBox(width: 8),
+            Text(label)
+          ],
+        ),
+      );
+    }
+
+    // 필터링된 부분 구성
+    List<Widget> buildInfraList(List<Infra> list) {
+      return list.map((infra) {
+        var results = PlaceUtils.convertDistance(infra.distance);
+
+        return ListTile(
+          leading: results['leadingIcon'],
+          title: Row(
+            children: <Widget>[
+              Expanded(
+                child: Text(
+                  infra.name,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Text(
+                '${results['minute']}분 (${infra.distance}km)',
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                  color: results['iconColor'],
+                ),
+              ),
+            ],
+          ),
+        );
+      }).toList();
+    }
 
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
@@ -41,98 +87,30 @@ class _NearbyInfraState extends State<NearbyInfra> {
               fillColor: Colors.blue.shade50,
               selectedColor: Colors.black,
               textStyle: TextStyle(
-                  fontSize: 18,
+                  fontSize: 16,
                   fontWeight: FontWeight.bold
               ),
               borderRadius: BorderRadius.all(Radius.circular(15)),
               borderWidth: 0,
               children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 1, horizontal: 50),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Image.asset(
-                        'asset/icon/hospital.png',
-                        width: 15,
-                      ),
-                      SizedBox(width: 15),
-                      Text('병원')
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 1, horizontal: 50),
-                  child: Row(
-                    children: [
-                      Image.asset(
-                        'asset/icon/park.png',
-                        width: 15,
-                      ),
-                      SizedBox(width: 15),
-                      Text('공원')
-                    ],
-                  ),
-                ),
+                // 병원
+                buttonContent('asset/icon/hospital.png', '병원'),
+                // 공원
+                buttonContent('asset/icon/park.png', '공원'),
+                // 학교
+                buttonContent('asset/icon/school.png', '학교'),
               ],
               onPressed: (int index) {
                 setState(() {
-                  _showHospitals = index == 0;
+                  _selectedType = InfraType.values[index];
                 });
               },
-              isSelected: [_showHospitals, !_showHospitals],
+              isSelected: InfraType.values.map((e) => e == _selectedType).toList(),
             ),
           ],
         ),
         SizedBox(height: 15),
-        ...List.generate(filteredList.length, (index) {
-          Infra infra = filteredList[index];
-          String transportType;
-          String time;
-          Icon leadingIcon;
-          Color iconColor;
-
-          if (infra.distance <= 1.5) {
-            int walkTime = (infra.distance / 4 * 60).round();
-            time = '$walkTime분';
-            transportType = '도보';
-            leadingIcon = Icon(Icons.directions_walk);
-            iconColor = Colors.green;
-          } else {
-            int driveTime = (infra.distance / 60 * 60).round();
-            time = '$driveTime분';
-            transportType = '차량';
-            leadingIcon = Icon(Icons.directions_car);
-            iconColor = Colors.blue;
-          }
-
-          return ListTile(
-            leading: Icon(
-              leadingIcon.icon,
-              color: iconColor,
-            ),
-            title: Row(
-              children: <Widget>[
-                Expanded(
-                  child: Text(
-                    infra.name,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                Text(
-                  '$time (${infra.distance}km)',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                    color: iconColor,
-                  ),
-                ),
-              ],
-            ),
-          );
-        }),
+        ...buildInfraList(filteredList),
         Divider(),
       ],
     );
