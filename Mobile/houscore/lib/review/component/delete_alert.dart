1fa0cc8897c5909efc9ee27a0222c41834097d2d
woojin <qpqpqp0102@naver.com>
2024-05-02 14:58:50 +0900

    Delete: delete_alert

--- Mobile/houscore/lib/review/component/delete_alert.dart
+++ Mobile/houscore/lib/review/component/delete_alert.dart
@@ -1,3 +1,3 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:rflutter_alert/rflutter_alert.dart';
+// import 'package:rflutter_alert/rflutter_alert.dart';
