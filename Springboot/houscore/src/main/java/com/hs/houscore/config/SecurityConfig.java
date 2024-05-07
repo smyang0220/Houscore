cdc7c4c457806a0e76fb640be296d03ad8d2d558
박영규 <dudrb5260@naver.com>
2024-05-07 09:46:49 +0900

    Feat: cookie 삭제 버전

--- Springboot/houscore/src/main/java/com/hs/houscore/config/SecurityConfig.java
+++ Springboot/houscore/src/main/java/com/hs/houscore/config/SecurityConfig.java
@@ -3,7 +3,7 @@ package com.hs.houscore.config;
 import com.hs.houscore.jwt.filter.JwtAuthenticationFilter;
 import com.hs.houscore.jwt.service.JwtService;
 import com.hs.houscore.postgre.service.MemberService;
-import com.hs.houscore.oauth2.HttpCookieOAuth2AuthorizationRequestRepository;
+import com.hs.houscore.oauth2.HttpOAuth2AuthorizationRequestRepository;
 import com.hs.houscore.oauth2.handler.OAuth2AuthenticationFailureHandler;
 import com.hs.houscore.oauth2.handler.OAuth2AuthenticationSuccessHandler;
 import com.hs.houscore.oauth2.service.CustomOAuth2MemberService;
@@ -33,20 +33,20 @@ public class SecurityConfig {
     private final CustomOAuth2MemberService customOAuth2MemberService;
     private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
     private final OAuth2AuthenticationFailureHandler oAuth2AuthenticationFailureHandler;
-    private final HttpCookieOAuth2AuthorizationRequestRepository httpCookieOAuth2AuthorizationRequestRepository;
+    private final HttpOAuth2AuthorizationRequestRepository httpOAuth2AuthorizationRequestRepository;
     private final JwtService jwtService;
 
     public SecurityConfig( // 순환문제 때문에 lazy 사용해서 생성자 만들기
                            CustomOAuth2MemberService customOAuth2MemberService,
                            OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler,
                            OAuth2AuthenticationFailureHandler oAuth2AuthenticationFailureHandler,
-                           HttpCookieOAuth2AuthorizationRequestRepository httpCookieOAuth2AuthorizationRequestRepository,
+                           HttpOAuth2AuthorizationRequestRepository httpOAuth2AuthorizationRequestRepository,
                            JwtService jwtService,
                            @Lazy MemberService memberService) {
         this.customOAuth2MemberService = customOAuth2MemberService;
         this.oAuth2AuthenticationSuccessHandler = oAuth2AuthenticationSuccessHandler;
         this.oAuth2AuthenticationFailureHandler = oAuth2AuthenticationFailureHandler;
-        this.httpCookieOAuth2AuthorizationRequestRepository = httpCookieOAuth2AuthorizationRequestRepository;
+        this.httpOAuth2AuthorizationRequestRepository = httpOAuth2AuthorizationRequestRepository;
         this.jwtService = jwtService;
     }
 
@@ -65,12 +65,12 @@ public class SecurityConfig {
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((requests) -> requests
                         .requestMatchers("/**","/oauth2/**", "/kakaologin/**", "/login/**",
-                                "/swagger-ui/**", "/v3/api-docs/**", "/login/**", "/api/**", "/swagger-resources/**", "/webjars/**").permitAll()
+                                "/swagger-ui/**", "/v3/api-docs/**", "/api/**", "/swagger-resources/**", "/webjars/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(sessions -> sessions.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .oauth2Login(configure ->
-                        configure.authorizationEndpoint(config -> config.authorizationRequestRepository(httpCookieOAuth2AuthorizationRequestRepository))
+                        configure.authorizationEndpoint(config -> config.authorizationRequestRepository(httpOAuth2AuthorizationRequestRepository))
                                 .userInfoEndpoint(config -> config.userService(customOAuth2MemberService))
                                 .successHandler(oAuth2AuthenticationSuccessHandler)
                                 .failureHandler(oAuth2AuthenticationFailureHandler)
