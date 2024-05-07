95faecc5fd7f0159a5be0d53a409f0d961e90d8f
woojin <qpqpqp0102@naver.com>
2024-05-07 13:43:40 +0900

    Feat: 건물 이름 입력, 점수/리뷰 - 리뷰 기반 점수

---
+++ Mobile/houscore/lib/residence/component/score_by_ai.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:houscore/residence/component/review_rating.dart';
+
+class ScoreByReview extends StatefulWidget {
+
+  @override
+  _ScoreByReviewState createState() =>
+      _ScoreByReviewState();
+}
+
+class _ScoreByReviewState
+    extends State<ScoreByReview> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          '리뷰 기반 점수',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+        Column(
+          children: [
+          ],
+        )
+      ],
+    );
+  }
+}
