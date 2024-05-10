3f690a3c69e4cd5539ff15ebce4e5e45711cf47a
woojin <qpqpqp0102@naver.com>
2024-05-10 14:05:10 +0900

    feat: review detail api test

--- Mobile/houscore/lib/member/component/kakao_login_button.dart
+++ Mobile/houscore/lib/member/component/kakao_login_button.dart
@@ -1,45 +1,51 @@
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:houscore/common/view/root_tab.dart';
-import 'package:houscore/member/model/kakao_login_token_model.dart';
-import 'package:houscore/member/repository/member_repository.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
-
 import '../../common/const/data.dart';
 import '../../common/secure_storage/secure_storage.dart';
-import '../../residence/repository/residence_repository.dart';
-
 class KakaoLoginButton extends ConsumerStatefulWidget {
   const KakaoLoginButton({Key? key}) : super(key: key);
-
   @override
   ConsumerState<KakaoLoginButton> createState() => _KakaoLoginButtonState();
 }
-
 class _KakaoLoginButtonState extends ConsumerState<KakaoLoginButton> {
   @override
   Widget build(BuildContext context) {
-    final dio = Dio();
-
     return Column(
       children: [
         InkWell(
           onTap: () async {
-            OAuthToken token;
-            KakaoLoginTokenModel resp;
             if (await isKakaoTalkInstalled()) {
               // 카카오가 설치됐을때
               try {
-                token = await UserApi.instance.loginWithKakaoTalk();
+                print(await KakaoSdk.origin);
+                OAuthToken token = await UserApi.instance.loginWithKakaoTalk();
+                print('로그인 성공');
+                print("엑세스토큰 ${token.accessToken}");
+                print("리프레시토큰 ${token.refreshToken}");
+                final refreshToken = token.accessToken;
+                final accessToken = token.refreshToken;
+                final storage = ref.read(secureStorageProvider);
+                await storage.write(
+                    key: REFRESH_TOKEN_KEY, value: refreshToken);
+                await storage.write(key: ACCESS_TOKEN_KEY, value: accessToken);
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (_) => RootTab(),
+                  ),
+                );
               } catch (error) {
                 print('로그인 실패 $error');
                 if (error is PlatformException && error.code == 'CANCELED') {
                   return;
                 }
                 try {
-                  token = await UserApi.instance.loginWithKakaoAccount();
+                  print(await KakaoSdk.origin);
+                  OAuthToken token =
+                  await UserApi.instance.loginWithKakaoAccount();
+                  print('로그인 성공 ${token.accessToken}');
                 } catch (error) {
                   throw Error();
                 }
@@ -47,71 +53,20 @@ class _KakaoLoginButtonState extends ConsumerState<KakaoLoginButton> {
             } else {
               // 카카오가 설치 안됐을때
               try {
-                token = await UserApi.instance.loginWithKakaoAccount();
+                print(await KakaoSdk.origin);
+                OAuthToken token =
+                await UserApi.instance.loginWithKakaoAccount();
+                print('로그인 성공 ${token.accessToken}');
               } catch (error) {
                 throw Error();
               }
             }
-            try {
-              final repository = ref.watch(MemberRepositroyProvider);
-
-              print("엑세스 ${token.accessToken}");
-              print("리프레시 ${token.refreshToken}");
-
-              resp = await repository.loginKakao(data: {
-                "accessToken": token.accessToken, // accessToken
-                "refreshToken": token.refreshToken, // refreshToken
-              });
-
-              print("전송후");
-              print(resp.accessToken);
-              print(resp.refreshToken);
-              print("출력후");
-              final accessToken = resp.accessToken;
-              final refreshToken = resp.refreshToken;
-
-              final storage = ref.read(secureStorageProvider);
-
-              await storage.write(key: REFRESH_TOKEN_KEY, value: refreshToken);
-              await storage.write(key: ACCESS_TOKEN_KEY, value: accessToken);
-              print("건물 검색 도과자");
-              final repository2 = ref.watch(residenceRepositoryProvider);
-              print("레포지토리 선언 완료");
-              final result2 = await repository2.getResidenceDetail(address: "서울특별시 강남구 개포동 12번지", lat: 37.49456430167525, lng: 127.07536876387186);
-              print("건물 검색 완료");
-              print(result2.newPlatPlc);
-
-
-
-
-              final result =
-                  await repository.searchMember("cnhug3@naver.com");
-
-              print("검색완료");
-              print(result[0].memberName);
-
-
-            } catch (e) {
-              print("안됨");
-            }
-            // print("리프레시 보내기 전");
-            // final repository = ref.watch(kakaoLoginRepositoryProvider);
-            // final resp = repository.refreshKakao();
-            // print("리프레시 보낸 후");
-            // print(resp['header']);
-
-            Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (_) => RootTab(),
-              ),
-            );
           },
 //thing to do
-
           child: Card(
             margin: const EdgeInsets.fromLTRB(20, 20, 20, 0),
             shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(7)),
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(7)),
             elevation: 2,
             child: Container(
               height: 50,
@@ -120,7 +75,7 @@ class _KakaoLoginButtonState extends ConsumerState<KakaoLoginButton> {
                 borderRadius: BorderRadius.circular(7),
               ), // BoxDecoration
               child:
-                  Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Row(mainAxisAlignment: MainAxisAlignment.center, children: [
                 Image.asset(
                   'asset/img/logo/kakao_login_medium_wide.png',
                   height: 30,
@@ -132,5 +87,43 @@ class _KakaoLoginButtonState extends ConsumerState<KakaoLoginButton> {
         ),
       ],
     );
+  }
+}
+Future<void> signInWithKakao(Ref ref) async {
+  if (await isKakaoTalkInstalled()) {
+    // 카카오가 설치됐을때
+    try {
+      print(await KakaoSdk.origin);
+      OAuthToken token = await UserApi.instance.loginWithKakaoTalk();
+      print('로그인 성공');
+      print("엑세스토큰 ${token.accessToken}");
+      print("리프레시토큰 ${token.refreshToken}");
+      final refreshToken = token.accessToken;
+      final accessToken = token.refreshToken;
+      final storage = ref.read(secureStorageProvider);
+      await storage.write(key: REFRESH_TOKEN_KEY, value: refreshToken);
+      await storage.write(key: ACCESS_TOKEN_KEY, value: accessToken);
+    } catch (error) {
+      print('로그인 실패 $error');
+      if (error is PlatformException && error.code == 'CANCELED') {
+        return;
+      }
+      try {
+        print(await KakaoSdk.origin);
+        OAuthToken token = await UserApi.instance.loginWithKakaoAccount();
+        print('로그인 성공 ${token.accessToken}');
+      } catch (error) {
+        throw Error();
+      }
+    }
+  } else {
+    // 카카오가 설치 안됐을때
+    try {
+      print(await KakaoSdk.origin);
+      OAuthToken token = await UserApi.instance.loginWithKakaoAccount();
+      print('로그인 성공 ${token.accessToken}');
+    } catch (error) {
+      throw Error();
+    }
   }
 }
