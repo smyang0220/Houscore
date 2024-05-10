51a9d2221e4722f695f4e68d389e279ab51fb0cd
박영규 <dudrb5260@naver.com>
2024-05-10 09:37:17 +0900

    Feat: Mainpage api 개발중

--- Springboot/houscore/src/main/java/com/hs/houscore/mongo/repository/BuildingRepository.java
+++ Springboot/houscore/src/main/java/com/hs/houscore/mongo/repository/BuildingRepository.java
@@ -1,6 +1,7 @@
 package com.hs.houscore.mongo.repository;
 
 import com.hs.houscore.mongo.entity.BuildingEntity;
+import org.bson.types.ObjectId;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -14,14 +15,12 @@ import java.util.Optional;
 @Repository
 public interface BuildingRepository extends MongoRepository<BuildingEntity, String> {
 
-    Optional<BuildingEntity> findById(@Param("id")Integer Id);
+    Optional<BuildingEntity> findById(@Param("id")ObjectId Id);
 
     Optional<BuildingEntity> findByNewPlatPlcOrPlatPlc(@Param("newPlatPlc")String newPlatPlc, @Param("PlatPlc")String PlatPlc);
 
-    List<BuildingEntity> findTop5ByNewPlatPlcContaining(@Param("newPlatPlc") String sigungu);
+    List<BuildingEntity> findAllBySigungu(@Param("sigungu") String sigungu);
 
     //시군구에 있는 모든 지역의 실거래가 평균 조회
-
     Page<BuildingEntity> findByBatchYn(@Param("batchYn")String batchYn, Pageable pageable);
-
 }
