0d892c3f3b1ebb50ea1f656b3630b4fe7df3a95b
ansangjun <yonggari0821@naver.com>
2024-05-10 13:44:25 +0900

    Feat : 상준 카카오로그인 해쉬 등록

--- Mobile/houscore/lib/myinfo/component/my_info.dart
+++ Mobile/houscore/lib/myinfo/component/my_info.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import '../../common/const/data.dart';
 import '../model/myinfo_model.dart';
 
 final secureStorageProvider = Provider<FlutterSecureStorage>((ref) => FlutterSecureStorage());
 
 final userInfoProvider = FutureProvider<MyinfoModel>((ref) async {
   final storage = ref.watch(secureStorageProvider);
-  String? name = await storage.read(key: "USER_NAME_KEY");
-  String? email = await storage.read(key: "USER_EMAIL_KEY");
+  String? name = await storage.read(key: USER_NAME_KEY);
+  String? email = await storage.read(key: USER_EMAIL_KEY);
+
+  // print('name : ${name}');
+  // print('email : ${name}');
 
   return MyinfoModel(
-    name: name ?? "알 수 없음",
-    email: email ?? "알 수 없음",
+    name: (name == null || name.isEmpty) ? "알 수 없음" : name,
+    email: (email == null || email.isEmpty) ? "알 수 없음" : email,
   );
 });
 
@@ -21,12 +25,11 @@ class MyInfo extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final userInfoAsyncValue = ref.watch(userInfoProvider);
 
-    print('user name : ${userInfoAsyncValue.value!.name}');
-    print('user email : ${userInfoAsyncValue.value!.email}');
+    // print('user name : ${userInfoAsyncValue.value!.name}');
+    // print('user email : ${userInfoAsyncValue.value!.email}');
 
     return userInfoAsyncValue.when(
-      data: (myInfo) {
-        String displayName = myInfo.name.isNotEmpty ? myInfo.name : "이름 없음";
+      data: (info) {
         return Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
@@ -40,8 +43,8 @@ class MyInfo extends ConsumerWidget {
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text(displayName, style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-                      Text(myInfo.email),
+                      Text(info.name, style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                      Text(info.email),
                     ],
                   ),
                 ),
