115aad93eb91baf7abfddfd2dbd8f5d328594b45
김희주 <mary2946@naver.com>
2024-05-12 20:38:17 +0900

    Fix: 리뷰 등록, 수정, 삭제 멤버 아이디 검증 추가 및 수정

---
+++ Springboot/houscore/src/main/java/com/hs/houscore/dto/ReviewDTO.java
@@ -0,0 +1,37 @@
+package com.hs.houscore.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+@Builder
+public class ReviewDTO {
+    private Long id;
+    private String address;
+    private String residenceType;
+    private Integer resideceFloor;
+    private String buildingName;
+    private StarRating starRating;
+    private String pros;
+    private String cons;
+    private String maintenanceCost;
+    private String images;
+    private String residenceYear;
+
+
+    @Data
+    public static class StarRating implements Serializable {
+        private Double traffic;
+        private Double building;
+        private Double inside;
+        private Double infra;
+        private Double security;
+
+        public double average() {
+            return (traffic + building + inside + infra + security) / 5.0;
+        }
+    }
+
+}
