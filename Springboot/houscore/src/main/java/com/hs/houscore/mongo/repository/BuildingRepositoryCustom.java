c4f4dfdf033fc62f22aa37c1dd7da28dc586758d
박영규 <dudrb5260@naver.com>
2024-05-13 13:01:14 +0900

    Merge: conflict 처리

--- Springboot/houscore/src/main/java/com/hs/houscore/mongo/repository/BuildingRepositoryCustom.java
+++ Springboot/houscore/src/main/java/com/hs/houscore/mongo/repository/BuildingRepositoryCustom.java
@@ -3,12 +3,9 @@ package com.hs.houscore.mongo.repository;
 import com.hs.houscore.mongo.entity.BuildingEntity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.aggregation.GeoNearOperation;
-import org.springframework.data.mongodb.core.aggregation.TypedAggregation;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.geo.GeoJsonPoint;
-import org.springframework.data.mongodb.core.aggregation.Aggregation;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
