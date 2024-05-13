eaa11c025b2f5ce279b05cf9992b814618a45ff6
안성재 <sammy0329@naver.com>
2024-05-13 13:45:29 +0900

    Feat: logs.log에 로그 적재되지 않도록 수정

--- ai/ai_model/main.py
+++ ai/ai_model/main.py
@@ -57,4 +57,4 @@ async def predict(request: PredictionRequest):
 
 
 if __name__ == '__main__':
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="info")
