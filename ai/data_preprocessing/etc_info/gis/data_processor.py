6392fc568359b17bc37173e06d1963f0f0a8549c
정종욱 <cnhug3@naver.com>
2024-05-16 16:03:14 +0900

    fix: gis모듈 최신화.
    gis 로직 모델 최신화 및 geocode docs, gis docs 추가.

--- ai/data_preprocessing/etc_info/gis/data_processor.py
+++ ai/data_preprocessing/etc_info/gis/data_processor.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import time
 
 def haversine_np(lon1, lat1, lon2, lat2):
@@ -6,6 +7,7 @@ def haversine_np(lon1, lat1, lon2, lat2):
     Haversine 공식을 이용해 지구 상의 두 점 사이의 거리를 계산합니다.
     입력은 모두 라디안 단위로 변환됩니다.
     """
+
     lon1, lat1, lon2, lat2 = map(np.radians, [lon1, lat1, lon2, lat2])
     dlon = lon2 - lon1
     dlat = lat2 - lat1
@@ -15,7 +17,7 @@ def haversine_np(lon1, lat1, lon2, lat2):
     km = 6371 * c  # 지구 반지름을 킬로미터 단위로 환산
     return km
 
-def filter_and_sort_buildings(center_coord, buildings_df, radius_km):
+def filter_and_sort_buildings(center_coard, buildings_df, radius_km):
     """
     주어진 반경 내의 건물들을 필터링하고 거리 순으로 정렬합니다.
     
@@ -27,29 +29,26 @@ def filter_and_sort_buildings(center_coord, buildings_df, radius_km):
     Returns:
     - DataFrame: 필터링 및 정렬된 건물 데이터
     """
-    center_lat, center_lon = center_coord
+    center_lat, center_lon = center_coard
     latitudes = buildings_df['latitude'].values
     longitudes = buildings_df['longitude'].values
 
-     # 거리 계산
+    # 모든 건물에 대해 거리 계산
     buildings_df['distance'] = haversine_np(center_lon, center_lat, longitudes, latitudes)
 
-    # 반경 내의 건물 필터링
+    # 반경 2km 이내의 건물 필터링
     filtered_df = buildings_df[buildings_df['distance'] <= radius_km]
 
-    # 거리 순으로 정렬
-    sorted_df = filtered_df.sort_values('distance')
+    # 가장 가까운 건물 찾기
+    nearest_related = filtered_df['distance'].min()
 
-    return sorted_df
+    return nearest_related
 
-def process_related_data(center_coord, df, radius_km):
-    related_number = 0
-    nearest_related = None
-    filtered_data = filter_and_sort_buildings(center_coord, df, radius_km)
-    related_number += len(filtered_data)
-    if len(filtered_data) > 0:
-        nearest_related = round(filtered_data.iloc[0]['distance'], 3)
-    return related_number, nearest_related
+def process_related_data(center_coard, df, radius_km):
+    nearest_distance = filter_and_sort_buildings(center_coard, df, radius_km)
+    nearest_related = round(nearest_distance, 3) if nearest_distance is not None else None
+    return nearest_related
+
 
 def analyze_area_data(star_data, other_dfs):
     results = []
@@ -57,9 +56,13 @@ def analyze_area_data(star_data, other_dfs):
 
     for idx, row in star_data.iterrows():
         file_start_time = time.time()
-        center_coord = (row['latitude'], row['longitude'])
-        result = [process_related_data(center_coord, df, 2) for df in other_dfs]
-        results.append([item for sublist in result for item in sublist])
+        if pd.notna(row['latitude']) and pd.notna(row['longitude']):
+            center_coard = (row['latitude'], row['longitude'])
+            result = [process_related_data(center_coard, df, 1) for df in other_dfs]
+            results.append(result)
+        else:
+            # 위도 또는 경도가 결측인 경우, 결과에 None 또는 적절한 기본값을 추가합니다.
+            results.append([None] * (len(other_dfs) * 2))  # 각 df에 대해 related_number와 nearest_related를 None으로 설정
         elapsed_time = time.time() - file_start_time
         print(f"Processed in {elapsed_time:.2f} seconds")
 
@@ -70,9 +73,14 @@ def analyze_area_data(star_data, other_dfs):
 
 def append_results_to_dataframe(df, results):
     # 초중고 분리시
-    categories = ['medical', 'subway', 'bus_stop', 'academy', 'laundry', 'market', 'park', 'library', 'kindergarden', 'elementary_school', 'middle_school', 'high_school', 'university']
+    categories = ['medical', 'subway', 'bus_stop', 'academy', 'laundry', 'market', 'park', 'library', 'school']
     for i, cat in enumerate(categories):
-        df[f'{cat}_related_number'] = [result[2*i] for result in results]
-        df[f'nearest_{cat}_related'] = [result[2*i+1] if result[2*i+1] is not None else 2.1 for result in results]
+        df[f'nearest_{cat}_related'] = [1 if len(result) <= 2*i+1 or not result else result[2*i+1] for result in results]
+    # Creating the 'nearest_transit_related' column using minimum of the two related columns
+    df['nearest_transit_related'] = df[['nearest_subway_related', 'nearest_bus_stop_related']].min(axis=1)
+
+    # Dropping the old columns
+    df.drop(columns=['nearest_subway_related', 'nearest_bus_stop_related'], inplace=True)
+
     return df
 
